664d22b280f2eec2959007c223ed9e7969223562
Supak Pukdam <48548611+supakjack@users.noreply.github.com>
2019-12-22 21:27:18 +0700

    final
    final

--- sha/src/app/pages/export/export.page.ts
+++ sha/src/app/pages/export/export.page.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-export',
   templateUrl: './export.page.html',
-  styleUrls: ['./export.page.scss'],
+  styleUrls: ['./export.page.scss']
 })
 export class ExportPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.wrap();
   }
 
+  ngOnInit() {}
+
+  wrap() {
+    location.href =
+      'https://10.80.39.17/TSP60/Municipality/index.php/sha/cycle3/frontend/Register_students_report/report_01';
+  }
 }
